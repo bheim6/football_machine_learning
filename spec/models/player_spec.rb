8acb90b03e423f4c08edc0fd53ab5c5a51278085
Brian Heim <bheim67@gmail.com>
2016-11-02 19:25:46 -0600

    added context to tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  it { should have_many :player_week_stats }
+  context "associations" do
+    it { should have_many :player_week_stats }
+  end
 end
--- spec/models/player_week_stat_spec.rb
+++ spec/models/player_week_stat_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PlayerWeekStat, type: :model do
-  it { should belong_to :player }
-  it { should belong_to :season_week }
+  context "associations" do
+    it { should belong_to :player }
+    it { should belong_to :season_week }
+  end
 end
