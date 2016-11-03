94fe372c6ea11dd0716c39ba96a0855d56f7f6fe
Brian Heim <bheim67@gmail.com>
2016-11-02 20:02:38 -0600

    add test for week prediction

--- spec/models/week_prediction_spec.rb
+++ spec/models/week_prediction_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WeekPrediction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should belong_to :player }
+    it { should belong_to :season_week }
+  end
 end
