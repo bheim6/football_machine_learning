e7e939a3f0582643418e671bab45d3b3338a1898
Jean Joeris <joeris.peter@gmail.com>
2016-11-03 09:58:07 -0600

    Add test for player week stats knowing stat names

--- app/models/player_week_stat.rb
+++ app/models/player_week_stat.rb
@@ -1,4 +1,14 @@
 class PlayerWeekStat < ApplicationRecord
   belongs_to :player
   belongs_to :season_week
+
+  def self.stat_names
+    stat_names ||= get_stats_names
+  end
+
+  private
+    def self.get_stats_names
+      stat_names = attribute_names - ["id", "player_id", "season_week_id", "created_at", "updated_at"]
+      stat_names.map { |stat_name| stat_name.tr("_", " ") }
+    end
 end
--- spec/models/player_week_stat_spec.rb
+++ spec/models/player_week_stat_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe PlayerWeekStat, type: :model do
     it { should belong_to :player }
     it { should belong_to :season_week }
   end
+
+  it "has stat name" do
+    stat_names = PlayerWeekStat.stat_names
+    expect_stat_names = [
+      "rush yds",
+      "rush att",
+      "rush td",
+      "rec yds",
+      "rec made",
+      "rec td",
+      "pass att",
+      "pass comp",
+      "pass yds",
+      "pass td",
+      "pass int",
+      "sacked"
+    ]
+    expect(stat_names).to eq(expect_stat_names)
+  end
 end
