149adfde634cfcf5d0dd929c280a4204564285e6
Jean Joeris <joeris.peter@gmail.com>
2016-11-03 09:36:37 -0600

    Add player stats table feature test, with factories

--- spec/factories/player_week_stats.rb
+++ spec/factories/player_week_stats.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :player_week_stat do
-    player nil
-    season_week nil
-    rush_yds 1
-    rush_att 1
-    rush_td 1
-    rec_yds 1
-    rec_made 1
-    rec_td 1
-    pass_att 1
-    pass_comp 1
-    pass_yds 1
-    pass_td 1
+    player
+    season_week
+    rush_yds 8
+    rush_att 2
+    rush_td 0
+    rec_yds 0
+    rec_made 0
+    rec_td 0
+    pass_att 40
+    pass_comp 25
+    pass_yds 289
+    pass_td 3
     pass_int 1
-    sacks 1
+    sacked 2
   end
 end
