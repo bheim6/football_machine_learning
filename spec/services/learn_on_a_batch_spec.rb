6af0195ed58a1966c4605c6a0caf0d9a2a353997
Jean Joeris <joeris.peter@gmail.com>
2016-11-08 13:42:55 -0700

    Forward and back prop with arbitrary batch size

--- spec/services/learn_on_a_batch_spec.rb
+++ spec/services/learn_on_a_batch_spec.rb
@@ -4,14 +4,14 @@ describe 'Neural net learns' do
   it 'given multiple sets of data simultainiously' do
     nn = NeuralNet.new(4,3,2)
 
-    correct_values = [[0.3,0.2],[0.6,0.5]]
-    nn.initial_activation = [[0.4,0.3,0.2,0.1],[0.7,0.6,0.5,0.4]]
+    correct_values = [[0.3,0.2],[0.6,0.5],[0.5,0.1]]
+    nn.initial_activation = [[0.4,0.3,0.2,0.1],[0.7,0.6,0.5,0.4],[0.3,0.8,0.9,0.12]]
 
     nn.forward_propagate
     nn.backward_propagate(correct_values)
     nn.forward_propagate
 
-    expect(nn.results.row_count).to eq(2)
+    expect(nn.results.row_count).to eq(3)
     expect(nn.results.column_count).to eq(2)
   end
 end
