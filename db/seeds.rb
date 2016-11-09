8fd9c76472c6b797654e8dbdb97f0aca3876fa77
Brian Heim <bheim67@gmail.com>
2016-11-09 12:43:01 -0700

    fixing seed file for only one year 2014

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 weeks = (1..17)
-seasons = (2014)
+season = 2014
 positions = ["QB", "RB", "WR", "TE"]
-seasons.each do |season|
+# seasons.each do |season|
   weeks.each do |week|
     positions.each do |position|
       FantasyAPIService.by_season_week({week: week, season: season, position: position})
       puts "#{position}s for week #{week} in season #{season} added"
     end
   end
-end
+# end
