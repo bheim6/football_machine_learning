bd8963bc818bcffd9103d87632f91fa848fd5d8b
Jean Joeris <joeris.peter@gmail.com>
2016-11-09 13:29:07 -0700

    try to make table entries optional

--- app/models/season_week.rb
+++ app/models/season_week.rb
@@ -1,4 +1,4 @@
 class SeasonWeek < ApplicationRecord
   has_many :player_week_stats, optional: true
-  has_many :players, through: :player_week_stats
+  has_many :players, through: :player_week_stats, optional: true
 end
